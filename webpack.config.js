b5b0a1cc26369f054e7b3a21d4efd43f35411f43
Alex Liang <alex.c.liang@gmail.com>
2017-12-12 14:34:07 -0500

    fixed externals

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ module.exports = env => ({
     path: path.resolve(__dirname,'dist'),
     filename: 'integ-grid-tools.js',
   },
+  externals: [
+    "data.maybe",
+    "fbjs",
+    "ramda",
+    "react",
+    "react-dom",
+    "styled-components"
+  ],
   module: {
   rules: [
     { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
