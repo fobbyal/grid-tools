52baa9a19f6ac4d1f5cc97f7f64c078b14ec56e0
Alex Liang <alex.c.liang@gmail.com>
2018-01-06 22:53:50 -0500

    added test for editEngine

--- src/editEngine.test.js
+++ src/editEngine.test.js
@@ -13,3 +13,33 @@ test('edit info should be dirty after add', () => {
   })
   expect(editEngine.isDirty(addedInfo)).toBe(true)
 })
+
+test('should add row properly', () => {
+  const editInfo = editEngine.generateInitialEditInfo()
+
+  const editedRow = {}
+  const addedInfo = editEngine.addRow({
+    editInfo,
+    editedRow,
+  })
+
+  expect(addedInfo.added.length).toBe(1)
+  expect(addedInfo.added[0]).toBe(editedRow)
+  expect(addedInfo.history.length).toBe(1)
+  expect(addedInfo.history[0]).toBe(editInfo)
+})
+test('should update row properly', () => {
+  const row1 = { a: 1, b: '2' }
+  const row1Updated = { a: 2, b: '2' }
+  const data = [row1]
+  const initialEdit = editEngine.generateInitialEditInfo()
+
+  const updated1 = editEngine.updateRow({
+    editInfo: initialEdit,
+    currentRow: row1,
+    editedRow: row1Updated,
+  })
+
+  console.log(updated1)
+
+})
