264dffbfecad923e3ddf42d45d596280c1d2f2f3
Alex Liang <alex.c.liang@gmail.com>
2018-05-06 17:00:11 -0400

    fixed package issues

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,13 @@ module.exports = env => ({
   },
   externals: [
     'data.maybe',
-    'fbjs',
-    'ramda',
     'react',
     'react-dom',
     'styled-components',
     'moment',
     'numeral',
+    'react-popper',
+    'react-virtualized',
   ],
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
